5a1275f3b2270afcd131eae20633e57cbf2c8177
Varun singh <942525@gmail.com>
2023-09-09 03:54:40 +0530

    gltf model added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import {motion} from 'framer-motion';
 import {styles} from '../styles'
 import { ComputersCanvas } from './canvas';
+import { DoubleSide } from 'three';
 
 
 const Hero = () => {
@@ -28,6 +29,9 @@ const Hero = () => {
         
         </div>
        <ComputersCanvas />
+       <div className='absolute
+
+       
       </section>
   )
 }
