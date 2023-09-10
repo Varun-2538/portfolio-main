323487f9158898542c45d9b137aeb01e76c04898
Varun singh <942525@gmail.com>
2023-09-10 20:26:17 +0530

    2nd last commint

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Smart Contract Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Trader",
       icon: creator,
     },
   ];
